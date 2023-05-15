749bf7fa64856adb0b98d993c8f210680d8bb072
Punkrig <111445872+Punkrig@users.noreply.github.com>
2023-05-14 21:07:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -62,6 +62,9 @@ function App() {
     setTodos((prevState)=>prevState.map((t)=>(t.id===data.id ? (t=data): t)));
 
   }
+  if(loading){
+    return <p>Carregando...</p>
+  }
   return (
     <div className="App">
       <div className="todo-header">
